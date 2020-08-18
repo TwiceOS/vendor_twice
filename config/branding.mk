ff3aeeb59569bb1f8e8f9b17480c2225cbdbc054
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2020-08-18 17:00:04 +0700

    branding: show warning if CURRENT_BUILD_TYPE not define

--- config/branding.mk
+++ config/branding.mk
@@ -1,30 +1,38 @@
-# Twice Version
-TWICE_PLATFORM_VERSION := 1.0
-
-# Set all versions
-TWICE_BUILD_TYPE := UNOFFICIAL
-CURRENT_BUILD_TYPE ?= nogapps
-
+# Define Var
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
-ifeq ($(CURRENT_BUILD_TYPE), nogapps)
-     TWICE_BUILD_ZIP_TYPE := VANILLA
-  else
-     TWICE_BUILD_ZIP_TYPE := GAPPS
-endif
+# Twice Version
+TWICE_PLATFORM_VERSION := 1.0
+TWICE_VERSION_PROP := $(TWICE_PLATFORM_VERSION)
 
+# Twice Official Release
 ifeq ($(TWICE_OFFICIAL), true)
    LIST = $(shell cat vendor/twice/twice.devices | awk '{ print $$1 }')
 
     ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-      IS_OFFICIAL=true
-      TWICE_BUILD_TYPE := OFFICIAL
+	IS_OFFICIAL=true
+	TWICE_BUILD_TYPE := OFFICIAL
     endif
     ifneq ($(IS_OFFICIAL), true)
-       TWICE_BUILD_TYPE := UNOFFICIAL
+	TWICE_BUILD_TYPE := UNOFFICIAL
     endif
+
+# Type of zip
+ifeq ($(CURRENT_BUILD_TYPE), nogapps)
+     TWICE_BUILD_ZIP_TYPE := VANILLA
+else ifeq ($(CURRENT_BUILD_TYPE), gapps)
+     TWICE_BUILD_ZIP_TYPE := GAPPS
+else
+    ifeq ($(CURRENT_BUILD_TYPE),)
+	$(warning "Twice vendor: CURRENT_BUILD_TYPE is undefined, assuming nogapps")
+    else
+	$(warning "Twice vendor: Incorrect value for CURRENT_BUILD_TYPE, forcing nogapps")
+    endif
+    TWICE_BUILD_ZIP_TYPE := VANILLA
+    CURRENT_BUILD_TYPE := nogapps
 endif
 
+# Set all versions
 TWICE_DATE_YEAR := $(shell date -u +%Y)
 TWICE_DATE_MONTH := $(shell date -u +%m)
 TWICE_DATE_DAY := $(shell date -u +%d)
@@ -36,7 +44,6 @@ TWICE_BUILD_DATE := $(TWICE_DATE_YEAR)$(TWICE_DATE_MONTH)$(TWICE_DATE_DAY)-$(TWI
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(TWICE_BUILD))
 
 TWICE_VERSION := TwiceOS-$(TWICE_BUILD)-$(TWICE_PLATFORM_VERSION)-$(TWICE_BUILD_DATE)-$(TWICE_BUILD_TYPE)-$(TWICE_BUILD_ZIP_TYPE)
-TWICE_VERSION_PROP := ten
 
 TWICE_PROPERTIES := \
     org.twice.version=$(TWICE_VERSION_PROP) \
