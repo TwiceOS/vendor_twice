Komodo-OS/vendor_komodo

overlay: G-VisualMod: merge SBH, URMM, URMVL, PGIOS - These are quashed commits from @Gnonymous7 as G-VisualMod creator, I intended to create good authorship culture. - PillGesture iOS as default
https://github.com/Komodo-OS-Rom/vendor_komodo/commit/fb5ced7221ae9280c34fd4a8205d00f1df52c37e
https://github.com/Komodo-OS-Rom/vendor_komodo/commit/b185e88c8a6abe69126eeea8301e52adb9ef8e33
https://github.com/Komodo-OS-Rom/vendor_komodo/commit/5e0b0b376cf00e668460f2e55c2c8accaaabc48e
https://github.com/Komodo-OS-Rom/vendor_komodo/commit/7941bca2059d592bea2247783ad1b0715a7177c6
https://github.com/Komodo-OS-Rom/vendor_komodo/commit/ea4e2fcc3514686ecd49f42ff20d524b3e63639b

Co-Authored-by: Kry9toN <dhimasbagusprayoga@gmail.com>
Change-Id: Ifbe53c59581740581438f6400a0d1a0251757008

config/common.mk
@@ -243,4 +243,7 @@ include vendor/komodo/config/accents.mk
 include vendor/komodo/config/primary.mk
 include vendor/komodo/config/font.mk
 
+#G-VisualMod
+include vendor/komodo/config/gvm.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

TwiceOS/vendor_twice

config/common.mk
@@ -169,4 +169,7 @@ include vendor/pixelstyle/config.mk
 # Customization
 include vendor/google-customization/config.mk
 
+#G-VisualMod
+include vendor/twice/config/gvm.mk
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk