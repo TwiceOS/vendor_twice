83a54f272e12f74f5c2c0d64df87ab0282124c67
MoveAngel <moveangel@minaproject.tech>
2020-09-21 16:09:16 +0000

    bacon: update logo when build finished
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -31,3 +31,32 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(TWICE_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(TWICE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(TWICE_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(TWICE_TARGET_PACKAGE)" >&2
+	@echo -e ""
+	@echo -e ${CL_RST}""${CL_RST}
+	@echo -e ${CL_YLW}"                    		     `//       "${CL_YLW}
+	@echo -e ${CL_YLW}"                               `-oydo       "${CL_YLW}
+	@echo -e ${CL_YLW}"                    `````..::/+oyhhhd:      "${CL_YLW}
+	@echo -e ${CL_YLW}"            `.:+oosssyyyyhdhhhhhhhhhy`      "${CL_YLW}
+	@echo -e ${CL_YLW}"        `-+syhhhhhhhhhhhhhhhhhhhhhhs.       "${CL_YLW}
+	@echo -e ${CL_YLW}"      .+yhhhhhhhhhhhhhhhhhhhhhhhhy/         "${CL_YLW}
+	@echo -e ${CL_YLW}"     .shhhyssooohhhhhhhhhyhyyyso/-          "${CL_YLW}
+	@echo -e ${CL_YLW}"   `ohho:.      hhhhhhhh````		       "${CL_YLW}
+	@echo -e ${CL_YLW}"  .yhh-        +hhhhhhh.         `:++. `::` "${CL_YLW}
+	@echo -e ${CL_YLW}" .hhd:        .dhhhhhd-         /hhhhho:dhd`"${CL_YLW}
+	@echo -e ${CL_YLW}" shhd`        +hhhhhd/          .yhhhhhhhhs "${CL_YLW}
+	@echo -e ${CL_YLW}" dhhh+       .dhhhhh+  `oshhs+`   yhhhhho:  "${CL_YLW}
+	@echo -e ${CL_YLW}"`dhhhhy/-`   /hhhhhy  /dhhhhhhh`  -dhhhd    "${CL_YLW}
+	@echo -e ${CL_YLW}" yhhhhhh+`   hhhhhd- .dhhy/hhhd-  -dhhhh    "${CL_YLW}
+	@echo -e ${CL_YLW}"  +hdy/`     dhhhhy  /dhd/`hhhd`  /hhhd/    "${CL_YLW}
+	@echo -e ${CL_YLW}"    .       .dhhhhy  -dhhoshhd-  `hhhhy     "${CL_YLW}
+	@echo -e ${CL_YLW}"            `hhhhhy   yhhhhhy-  .yhhhy`     "${CL_YLW}
+	@echo -e ${CL_YLW}"             +dhhhho-`-dhhhy-.:ohhhho       "${CL_YLW}
+	@echo -e ${CL_YLW}"              /ydhhhhhhhsyhhhhhhhdy:        "${CL_YLW}
+	@echo -e ${CL_YLW}"               `-/osos/.``/osyso+-`         "${CL_YLW}
+	@echo -e ${CL_RST}""${CL_RST}
+	@echo -e ${CL_YLW}"     ______         _           ____  _____ "${CL_YLW}
+	@echo -e ${CL_YLW}"    /_  __/      __(_)_______  / __ \/ ___/ "${CL_YLW}
+	@echo -e ${CL_CYN}"     / / | | /| / / / ___/ _ \/ / / /\__ \  "${CL_CYN}
+	@echo -e ${CL_CYN}"    / /  | |/ |/ / / /__/  __/ /_/ /___/ /  "${CL_CYN}
+	@echo -e ${CL_CYN}"   /_/   |__/|__/_/\___/\___/\____//____/   "${CL_CYN}
+	@echo -e ${CL_RST}""${CL_RST}
