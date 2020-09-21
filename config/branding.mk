rio-31/vendor_komodo

branding: make simple declaration
Add new prop

Change-Id: If63f2f4253f1829891f9b2547e47535a28a2ccfe

config/branding.mk
@@ -3,7 +3,7 @@ CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 # Komodo Version
 KOMODO_PLATFORM_VERSION := 2.7
-KOMODO_VERSION_PROP := $(KOMODO_PLATFORM_VERSION)
+KOMODO_VERSION_NAME := NKRI75
 
 # Set all versions
 KOMODO_DATE_YEAR := $(shell date -u +%Y)
@@ -62,13 +62,14 @@ TARGET_PRODUCT_SHORT := $(subst aosp_,,$(KOMODO_BUILD))
 ROM_FINGERPRINT := KomodoOS/$(KOMODO_PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(KOMODO_BUILD_DATE)
 
 KOMODO_PROPERTIES := \
-    org.komodo.version=$(KOMODO_VERSION_PROP) \
+    org.komodo.version=$(KOMODO_PLATFORM_VERSION) \
     org.komodo.version.display=$(KOMODO_VERSION) \
     org.komodo.build_date=$(KOMODO_BUILD_DATE) \
     org.komodo.build_date_utc=$(KOMODO_BUILD_DATE_UTC) \
     org.komodo.build_type=$(KOMODO_BUILD_TYPE) \
     org.komodo.ziptype=$(KOMODO_BUILD_ZIP_TYPE) \
-    org.komodo.fingerprint=$(ROM_FINGERPRINT)
+    org.komodo.fingerprint=$(ROM_FINGERPRINT) \
+    org.komodo.version.name=$(KOMODO_VERSION_NAME)
 
 # Variable file name for jenkins
 $(info) $(shell echo $(KOMODO_VERSION) > $(OUT_DIR)/var-file_name)

TwiceOS/vendor_twice

Signed-off-by: MoveAngel <moveangel@minaproject.tech>

config/branding.mk
@@ -3,7 +3,7 @@ CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 # Twice Version
 TWICE_PLATFORM_VERSION := 1.0
-TWICE_VERSION_PROP := $(TWICE_PLATFORM_VERSION)
+TWICE_VERSION_NAME := TheStoryBegins
 
 # Twice Official Release
 ifeq ($(TWICE_OFFICIAL), true)
@@ -46,10 +46,11 @@ TARGET_PRODUCT_SHORT := $(subst aosp_,,$(TWICE_BUILD))
 TWICE_VERSION := TwiceOS-$(TWICE_BUILD)-$(TWICE_PLATFORM_VERSION)-$(TWICE_BUILD_DATE)-$(TWICE_BUILD_TYPE)-$(TWICE_BUILD_ZIP_TYPE)
 
 TWICE_PROPERTIES := \
-    org.twice.version=$(TWICE_VERSION_PROP) \
+    org.twice.version=$(TWICE_PLATFORM_VERSION) \
     org.twice.version.display=$(TWICE_VERSION) \
     org.twice.build_date=$(TWICE_BUILD_DATE) \
     org.twice.build_date_utc=$(TWICE_BUILD_DATE_UTC) \
     org.twice.build_type=$(TWICE_BUILD_TYPE) \
-    org.twice.ziptype=$(TWICE_BUILD_ZIP_TYPE)
+    org.twice.ziptype=$(TWICE_BUILD_ZIP_TYPE) \
+    org.twice.version.name=$(TWICE_VERSION_NAME)
 